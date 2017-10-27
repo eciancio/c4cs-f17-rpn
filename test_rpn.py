581a2fe55d30065c6a737e5be0ae99a3cad98d49
Your Name <you@example.com>
2017-10-27 16:28:11 +0000

    Add test for subtract and impliment subtract

--- test_rpn.py
+++ test_rpn.py
@@ -4,6 +4,9 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rnp.calculate('1 1 + ')
+        result = rpn.calculate('1 1 + ')
         self.assertEqual(2, result)
-    
+        
+    def test_subtract(self):
+        result = rpn.calculate('5 3 - ')
+        self.assertEqual(2, result)
